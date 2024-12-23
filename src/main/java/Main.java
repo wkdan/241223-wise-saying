9d448b8bbe2fa81cb53bd0350cbe839aad71a9f3
wkdan <jmy8876@gmail.com>
2024-12-23 15:19:43 +0900

    feat: 2단계 요구사항 구현
    - 명언, 작가 입력부 구현

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         App app = new App();
@@ -9,7 +11,15 @@ public class Main {
 class App {
 
     public void run() {
+
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("== 명언 앱 ==");
+        System.out.print("명언 : ");
+        scanner.nextLine(); // 입력값 가져옴. 입력값이 없으면 대기
+        System.out.print("작가 : ");
+        scanner.nextLine();
+        System.out.println("1번 명언이 등록되었습니다.");
         System.out.println("명령) 종료");
     }
 }
