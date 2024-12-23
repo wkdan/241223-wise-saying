043512bdf46798b6c3103d09f686129f3da21aed
wkdan <jmy8876@gmail.com>
2024-12-23 15:34:08 +0900

    feat: 3단계 요구사항 구현
    - 명령 입력받는 로직 구현

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,11 +15,21 @@ class App {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("== 명언 앱 ==");
-        System.out.print("명언 : ");
-        scanner.nextLine(); // 입력값 가져옴. 입력값이 없으면 대기
-        System.out.print("작가 : ");
-        scanner.nextLine();
-        System.out.println("1번 명언이 등록되었습니다.");
-        System.out.println("명령) 종료");
+
+        while (true) {
+
+            System.out.print("명령) ");
+            String command = scanner.nextLine();
+
+            if (command.equals("등록")) {
+                System.out.print("명언 : ");
+                scanner.nextLine(); // 입력값 가져옴. 입력값이 없으면 대기
+
+                System.out.print("작가 : ");
+                scanner.nextLine();
+
+                System.out.println("1번 명언이 등록되었습니다.");
+            }
+        }
     }
 }
