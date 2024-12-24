bec7cf9c0b336c88aa99fbd43bd18278e5e41bf8
wkdan <jmy8876@gmail.com>
2024-12-24 10:18:43 +0900

    refactor : 5단계 요구사항 수정중
    - 해시 테이블을 사용한 구조 -> 객체를 사용한 구조 변경중

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,7 +16,9 @@ class App {
         Map<Integer, String[]> map = new HashMap<>();
         Scanner scanner = new Scanner(System.in);
         System.out.println("== 명언 앱 ==");
-        int lastNo = 1;
+        int lastNo = 0;
+
+        WiseSaying wiseSaying = new WiseSaying();
 
         while (true) {
 
@@ -29,31 +31,34 @@ class App {
 
             } else if (command.equals("등록")) {
                 System.out.print("명언 : ");
-                String wise = scanner.nextLine(); // 입력값 가져옴. 입력값이 없으면 대기
+                String content = scanner.nextLine(); // 입력값 가져옴. 입력값이 없으면 대기
 
                 System.out.print("작가 : ");
                 String author = scanner.nextLine();
 
-                map.put(lastNo, new String[]{author, wise});
+
+
+                wiseSaying.id =  ++lastNo;
+                wiseSaying.content = content;
+                wiseSaying.author = author;
+
 
                 System.out.println("%d번 명언이 등록되었습니다.".formatted(lastNo++));
 
-
             } else if (command.equals("목록")) {
-                System.out.println("번호 / "+"작가 / "+"명언 / ");
+                System.out.println("번호 / 작가 / 명언 /");
                 System.out.println("----------------------");
 
-                for (Map.Entry<Integer, String[]> entry : map.entrySet()) {
-                    int no = entry.getKey();
-                    String[] data = entry.getValue();
+                System.out.println("%d / %s / %s".formatted(wiseSaying.id, wiseSaying.author, wiseSaying.content));
 
-                    String author = data[0];
-                    String wise = data[1];
 
-                    System.out.println(no+" / "+author+" / "+wise);
-                }
             }
-
         }
     }
+}
+
+class WiseSaying {
+    int id;
+    String content;
+    String author;
 }
